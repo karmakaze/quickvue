8df72b4a648777ddd6dfddf53faa3d791707c530
Keith Kim <karmakaze@users.noreply.github.com>
2018-05-21 16:13:33 -0400

    Have update-server.sh do a 'npm build' on server only

--- update-server.sh
+++ update-server.sh
@@ -4,6 +4,7 @@ cd "`dirname $0`"
 
 if [ "`hostname`" == "vps106089" ]; then
   ./git-pull.sh
+  npm build
 else
-  ssh -t keith@vps106089.vps.ovh.ca './vue-statuspages/git-pull.sh'
+  ssh -t keith@vps106089.vps.ovh.ca './vue-statuspages/update-server.sh'
 fi
