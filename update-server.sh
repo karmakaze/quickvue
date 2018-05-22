379d7a8e0c0166720ff9aa27468494e6c1015d4f
Keith Kim <karmakaze@users.noreply.github.com>
2018-05-22 05:38:47 +0000

    Restart java service after 'npm build' to clear caching

--- update-server.sh
+++ update-server.sh
@@ -5,6 +5,7 @@ cd "`dirname $0`"
 if [ "`hostname`" == "vps106089" ]; then
   ./git-pull.sh
   npm build
+  ../statuspages-service/restart-service.sh
 else
   ssh -t keith@vps106089.vps.ovh.ca './vue-statuspages/update-server.sh'
 fi
