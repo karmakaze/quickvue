fb31002fb075b9e60ade1aeb784e3210960af47e
Keith Kim <karmakaze@users.noreply.github.com>
2018-05-22 05:46:29 +0000

    Fix update-server.sh 'npm build' didn't work using 'node build/build.js'

--- update-server.sh
+++ update-server.sh
@@ -4,7 +4,7 @@ cd "`dirname $0`"
 
 if [ "`hostname`" == "vps106089" ]; then
   ./git-pull.sh
-  npm build
+  node build/build.js
   ../statuspages-service/restart-service.sh
 else
   ssh -t keith@vps106089.vps.ovh.ca './vue-statuspages/update-server.sh'
