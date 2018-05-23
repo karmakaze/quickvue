4cd4e531a204bd9e4414ec4f725fc611457d55f2
Keith Kim <karmakaze@users.noreply.github.com>
2018-05-23 04:43:55 +0000

    Stop the java service before doing 'node build/build.js' as it wants the CPU/memory

--- update-server.sh
+++ update-server.sh
@@ -4,6 +4,7 @@ cd "`dirname $0`"
 
 if [ "`hostname`" == "vps106089" ]; then
   ./git-pull.sh
+  ../statuspages-service/stop-service.sh
   node build/build.js
   ../statuspages-service/restart-service.sh
 else
