0ff384a5e857c471093a7326addeb49327a474ee
Keith Kim <karmakaze@users.noreply.github.com>
2018-07-29 23:15:01 -0400

    Update README.md and update-server.sh

--- update-server.sh
+++ update-server.sh
@@ -4,9 +4,9 @@ cd "`dirname $0`"
 
 if [ "`hostname`" == "vps106089" ]; then
   ./git-pull.sh
-  ../statuspages-service/stop-service.sh
-  node build/build.js
-  ../statuspages-service/restart-service.sh
+  echo "Need to run (with more free memory): node build/build.js"
 else
-  ssh -t keith@vps106089.vps.ovh.ca './vue-statuspages/update-server.sh'
+  ssh -t keith@vps106089.vps.ovh.ca './hn-redux/git-pull.sh'
+  node build/build.js
+  rsync -avz docs keith@vps106089.vps.ovh.ca:hn-redux/docs
 fi
