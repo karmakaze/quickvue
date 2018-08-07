d15ae1af2cd2b7c68fd8d5e5228493d2e29137ad
Keith Kim <karmakaze@users.noreply.github.com>
2018-08-07 01:23:35 -0400

    Remove hn-redux-l

--- hn-redux-l/src/actions/api/labels/index.cr
+++
@@ -1,9 +0,0 @@
-# in src/actions/api/labels/index.cr
-class Api::Labels::Index < ApiAction
-  param page : Int32 = 1
-
-  get "/types/:type/labels" do |type|
-    labels = [{ type: type, name: "my-label" }]
-    json(labels, Status::OK)
-  end
-end
--- hn-redux-l/src/actions/api/labels/show.cr
+++
@@ -1,6 +0,0 @@
-# in src/actions/api/labels/show.cr
-class Api::Labels::Show < ApiAction
-  get "/types/:type/labels/:name" do |type, name|
-    json({ type: type, name: name }, Status::OK)
-  end
-end
