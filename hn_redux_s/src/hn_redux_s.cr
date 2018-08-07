f5e51cb80d4d0d2af1db2de9cd98187dea518b25
Keith Kim <karmakaze@users.noreply.github.com>
2018-08-06 22:57:22 -0400

    Set up hot-reload with nodemon; make test GET / endpoint

--- hn_redux_s/src/hn_redux_s.cr
+++ hn_redux_s/src/hn_redux_s.cr
@@ -1,6 +1,29 @@
+require "kemal"
 require "./hn_redux_s/*"
 
 # TODO: Write documentation for `HnReduxS`
 module HnReduxS
-  # TODO: Put your code here
+  get "/:id" do |env|
+  # .. show something ..
+    puts "env.params #{env.params.inspect}"
+    "Hello, world.\n"
+  end
+
+  # post "/" do
+  # .. create something ..
+  # end
+
+  # put "/" do
+  # .. replace something ..
+  # end
+
+  # patch "/" do
+  # .. modify something ..
+  # end
+
+  # delete "/" do
+  # .. annihilate something ..
+  # end
 end
+
+Kemal.run
